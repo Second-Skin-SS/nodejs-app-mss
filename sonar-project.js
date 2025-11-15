e8af0d1ae72c4100c7218b80fc51e010b816e00c
manojreddy <153477081+ManojReddypersonal@users.noreply.github.com>
2025-11-15 18:38:10 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -7,7 +7,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsMSS',
-	    'sonar.login': ' squ_c269ef58a4a5e6bca637bea3543583df6a090504',
+	    'sonar.login': 'squ_c269ef58a4a5e6bca637bea3543583df6a090504',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
             'sonar.sourceEncoding':'UTF-8',
